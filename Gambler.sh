974b1b3ee424be7b9dbdc0319ff8348271cf6dff
Ankit Bhat <ankitbhatpro@gmail.com>
2020-06-09 12:13:06 +0530

    ADDED: Dictionary to store days won and lost

--- Gambler.sh
+++ Gambler.sh
@@ -1,15 +1,15 @@
 #!/bin/bash -x
 stake=100;
 bet=1;
-win=0;
-loss=0;
 newAmount=100;
+declare -A win_array
+declare -A loss_array
 TotalAmount=0;
-for (( i=0;i<20;i++ ))
+for (( i=1;i<=20;i++ ))
 do
 	while [ $newAmount -lt 150 -a $newAmount -gt 50 ]
 	do
-	random=$(( $RANDOM%2 ));
+	random=$(( (($RANDOM%100))%2 ));
 	if [[ $random -eq 1 ]]
 		then
 		newAmount=$(( $newAmount+1 ));
@@ -20,7 +20,14 @@ do
 	fi
 	done
 	TotalAmount=$(( $TotalAmount+$newAmount ))
-	newAmount=100;
+	if [ $newAmount -ge 100 ]
+		then
+		win_array[Day$i]="Won by $(( $newAmount-100 )) dollars"
+		else
+		loss_array[Day$i]="Lost by $(( 100-$newAmount )) dollars"
+	fi 
+		newAmount=100;
+
 done
 if [ $TotalAmount -gt 2000 ]
 	then
@@ -28,3 +35,12 @@ if [ $TotalAmount -gt 2000 ]
 	else
 	echo Total Amount Lost = $(( 2000-$TotalAmount ))
 fi
+for element in ${!win_array[@]}
+do
+set +x
+echo $element "-" ${win_array[$element]}
+done
+for element in ${!loss_array[@]}
+do
+echo $element "-" ${loss_array[$element]}
+done
