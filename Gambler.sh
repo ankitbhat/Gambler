bfb3a053da8be430e416e636d68f7b5105aca9e2
Ankit Bhat <ankitbhatpro@gmail.com>
2020-06-09 11:16:47 +0530

    ADDED: Condition to calculate Total Amount won or lost after 20 days

--- Gambler.sh
+++ Gambler.sh
@@ -4,15 +4,27 @@ bet=1;
 win=0;
 loss=0;
 newAmount=100;
-while [ $newAmount -lt 150 -a $newAmount -gt 50 ]
+TotalAmount=0;
+for (( i=0;i<20;i++ ))
 do
-random=$(( $RANDOM%2 ));
-if [[ $random -eq 1 ]]
-	then
-	newAmount=$(( $newAmount+1 ));
-	echo Amount Left = $newAmount
-	else
-	newAmount=$(( $newAmount-1 )); 
-	echo Amount Left= $newAmount
-fi
+	while [ $newAmount -lt 150 -a $newAmount -gt 50 ]
+	do
+	random=$(( $RANDOM%2 ));
+	if [[ $random -eq 1 ]]
+		then
+		newAmount=$(( $newAmount+1 ));
+		echo Amount Left = $newAmount
+		else
+		newAmount=$(( $newAmount-1 )); 
+		echo Amount Left= $newAmount
+	fi
+	done
+	TotalAmount=$(( $TotalAmount+$newAmount ))
+	newAmount=100;
 done
+if [ $TotalAmount -gt 2000 ]
+	then
+	echo Total Amount Won = $(( $TotalAmount-2000 ))
+	else
+	echo Total Amount Lost = $(( 2000-$TotalAmount ))
+fi
